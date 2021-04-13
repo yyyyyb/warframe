fa55692697f7d45d851549afdc969c2c9eeac63b
yyyyyb <605490705@qq.com>
2021-04-13 14:30:20 +0800

    commit

--- src/main/java/com/zyb/warframe/controller/LoginController.java
+++ src/main/java/com/zyb/warframe/controller/LoginController.java
@@ -3,6 +3,8 @@ package com.zyb.warframe.controller;
 import com.zyb.warframe.Result;
 import com.zyb.warframe.pojo.User;
 import com.zyb.warframe.service.UserService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,12 +13,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.util.HtmlUtils;
 
+import java.util.logging.Logger;
+
 
 @Controller
 public class LoginController {
     @Autowired
     UserService userService;
 
+    protected final Log logger = LogFactory.getLog(this.getClass());
     @CrossOrigin
     @PostMapping(value = "api/login")
     @ResponseBody
@@ -25,11 +30,13 @@ public class LoginController {
         username = HtmlUtils.htmlEscape(username);
 
         User user = userService.get(username, requestUser.getPassWord());
+        logger.info("登录的用户" + username);
         if (null == user) {
+            logger.info("登录失败");
             return new Result(400, "失败");
         } else {
+            logger.info("登录成功");
             return new Result(200, "成功");
         }
-
     }
 }
